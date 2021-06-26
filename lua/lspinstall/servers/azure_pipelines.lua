af8b002cf5654d64a7c275ec4de528a45f4c85f6
Axel <axel_so@msn.com>
2021-06-25 19:15:24 -0500

    Add azure_pipelines.lua file

---
+++ lua/lspinstall/servers/azure_pipelines.lua
@@ -0,0 +1,9 @@
+local config = require"lspinstall/util".extract_config("azure_pipelines_ls")
+config.default_config.cmd[1] = "./node_modules/.bin/azure-pipelines-language-server"
+
+return vim.tbl_extend('error', config, {
+  install_script = [[
+  ! test -f package.json && yarn init -y  || true
+  yarn add azure-pipelines-language-server@latest
+  ]]
+})
