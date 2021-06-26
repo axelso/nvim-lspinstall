c0399a722bcf189dcab580afe1b8cd03a13011fa
Axel <axel_so@msn.com>
2021-06-25 20:12:59 -0500

    Change command server configuration for azure-pipelines

--- lua/lspinstall/servers/azure_pipelines.lua
+++ lua/lspinstall/servers/azure_pipelines.lua
@@ -1,9 +1,17 @@
 local config = require"lspinstall/util".extract_config("azure_pipelines_ls")
-config.default_config.cmd[1] = "node ./node_modules/azure-pipelines-language-server/out/server.js"
+-- config.default_config.cmd[1] = "./node_modules/azure-pipelines-language-server/out/server.js"
 
 return vim.tbl_extend('error', config, {
   install_script = [[
   ! test -f package.json && yarn init -y  || true
   yarn add azure-pipelines-language-server@latest
-  ]]
+  ]],
+  default_config = {
+    cmd = { "node", "./node_modules/azure-pipelines-language-server/out/server.js", "--stdio" },
+    filetypes = { 'yaml' },
+    root_dir = require'lspconfig'.util.root_pattern(".git", vim.fn.getcwd()),
+    init_options = {
+      provideFormatter = true,
+    },
+  }
 })
